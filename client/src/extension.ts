42b451a31acef8d95eb8cc1ba3f5f78bd4771d52
Mark Stowell <markstowell15@gmail.com>
2022-11-07 07:53:55 -0500

    changed path for executable in client

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,44 +1,47 @@
-import { ExtensionContext, OutputChannel, workspace } from 'vscode';
+import { ExtensionContext, OutputChannel, workspace } from "vscode";
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	SocketTransport,
-	Executable,
-	TransportKind,
-	ExecutableOptions
-} from 'vscode-languageclient/node';
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  SocketTransport,
+  Executable,
+  TransportKind,
+  ExecutableOptions,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	const transport: SocketTransport = { kind: TransportKind.socket, port: 8080 };
-	// const options: ExecutableOptions = { detached: true, shell: true };
-	const unicon: Executable = { command: '/home/mark/Documents/indep-study-2022/tcp/unicon-lsp-server', transport: transport };
-	const serverOptions: ServerOptions = {
-		run: unicon,
-		debug: unicon
-	};
+  const transport: SocketTransport = { kind: TransportKind.socket, port: 8080 };
+  // const options: ExecutableOptions = { detached: true, shell: true };
+  const unicon: Executable = {
+    command: "../../server/unicon-lsp-server",
+    transport: transport,
+  };
+  const serverOptions: ServerOptions = {
+    run: unicon,
+    debug: unicon,
+  };
 
-	const clientOptions: LanguageClientOptions = { 
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc' files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
-	client = new LanguageClient(
-		'uniconClient',
-		'Unicon Client',
-		serverOptions,
-		clientOptions
-	);
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc' files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+  client = new LanguageClient(
+    "uniconClient",
+    "Unicon Client",
+    serverOptions,
+    clientOptions
+  );
 
-	client.start();
+  client.start();
 }
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
