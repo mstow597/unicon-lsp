012d3b8c2364888775cbd162faa37dde3dc45e7a
HereBeCode <80660221+HereBeCode@users.noreply.github.com>
2022-12-22 15:17:32 -0500

    Update extension.ts

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -21,7 +21,7 @@ let client: LanguageClient;
 export function activate(context: ExtensionContext) {
 	const transport: SocketTransport = { kind: TransportKind.socket, port: 8080 };
 	// const options: ExecutableOptions = { detached: true, shell: true };
-	const unicon: Executable = { command: '/home/mark/Documents/unicon-lsp/server/unicon-lsp-server', transport: transport };
+	const unicon: Executable = { command: '/home/mark/Documents/unicon/uni/ulsp/launch-lsp', transport: transport };
 	const serverOptions: ServerOptions = {
 		run: unicon,
 		debug: unicon
